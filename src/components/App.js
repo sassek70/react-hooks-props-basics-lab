dc662a88ceb1188f2aeb24fcf732b4a5a6ef2bd3
Kevin Sasse <sassek70@gmail.com>
2022-10-11 23:19:05 -0400

    commit tree a775b81be853c1b75ce6aaae6e1412f16379c246

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,13 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home city={user.city} color={user.color} name={user.name} />
+      <About bio={user.bio} linkedin={user.links.linkedin} github={user.links.github}/>
     </div>
   );
 }
